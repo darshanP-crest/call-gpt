98e3a9859086ef9e9a080c7c8d9b1bf503f16a6c
darshanP-crest <darshan.p@crestinfosystems.com>
2024-06-27 22:27:32 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,160 @@
+require("dotenv").config();
+require("colors");
+
+const express = require("express");
+const ExpressWs = require("express-ws");
+
+const { GptService } = require("./services/gpt-service");
+const { StreamService } = require("./services/stream-service");
+const { TranscriptionService } = require("./services/transcription-service");
+const { TextToSpeechService } = require("./services/tts-service");
+const { recordingService } = require("./services/recording-service");
+const { makeOutBoundCall } = require("./scripts/outbound-call");
+
+const VoiceResponse = require("twilio").twiml.VoiceResponse;
+
+const app = express();
+app.use(express.json());
+ExpressWs(app);
+
+const PORT = process.env.PORT || 3000;
+
+app.post("/incoming", (req, res) => {
+  try {
+    const response = new VoiceResponse();
+    const connect = response.connect();
+    connect.stream({ url: `wss://${process.env.SERVER}/connection` });
+
+    res.type("text/xml");
+    res.end(response.toString());
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.ws("/connection", (ws) => {
+  try {
+    ws.on("error", console.error);
+    // Filled in from start message
+    let streamSid;
+    let callSid;
+
+    const gptService = new GptService();
+    const streamService = new StreamService(ws);
+    const transcriptionService = new TranscriptionService();
+    const ttsService = new TextToSpeechService({});
+
+    let marks = [];
+    let interactionCount = 0;
+
+    // Incoming from MediaStream
+    ws.on("message", function message(data) {
+      const msg = JSON.parse(data);
+      // console.log("🚀 ~ message ~ msg:", msg)
+      if (msg.event === "start") {
+        streamSid = msg.start.streamSid;
+        callSid = msg.start.callSid;
+
+        streamService.setStreamSid(streamSid);
+        gptService.setCallSid(callSid);
+
+        // Set RECORDING_ENABLED='true' in .env to record calls
+        recordingService(ttsService, callSid).then(() => {
+          console.log(
+            `Twilio -> Starting Media Stream for ${streamSid}`.underline.red
+          );
+          ttsService.generate(
+            {
+              partialResponseIndex: null,
+              partialResponse:
+                "Hello! I understand you're looking for a pair of AirPods, is that correct?",
+            },
+            0
+          );
+        });
+      } else if (msg.event === "media") {
+        transcriptionService.send(msg.media.payload);
+      } else if (msg.event === "mark") {
+        const label = msg.mark.name;
+        console.log(
+          `Twilio -> Audio completed mark (${msg.sequenceNumber}): ${label}`.red
+        );
+        marks = marks.filter((m) => m !== msg.mark.name);
+      } else if (msg.event === "stop") {
+        console.log(`Twilio -> Media stream ${streamSid} ended.`.underline.red);
+      }
+    });
+
+    transcriptionService.on("utterance", async (text) => {
+      // This is a bit of a hack to filter out empty utterances
+      if (marks.length > 0 && text?.length > 5) {
+        console.log("Twilio -> Interruption, Clearing stream".red);
+        ws.send(
+          JSON.stringify({
+            streamSid,
+            event: "clear",
+          })
+        );
+      }
+    });
+
+    transcriptionService.on("transcription", async (text) => {
+      if (!text) {
+        return;
+      }
+      console.log(
+        `Interaction ${interactionCount} – STT -> GPT: ${text}`.yellow
+      );
+      gptService.completion(text, interactionCount);
+      interactionCount += 1;
+    });
+
+    gptService.on("gptreply", async (gptReply, icount) => {
+      console.log(
+        `Interaction ${icount}: GPT -> TTS: ${gptReply.partialResponse}`.green
+      );
+      ttsService.generate(gptReply, icount);
+    });
+
+    ttsService.on("speech", (responseIndex, audio, label, icount) => {
+      console.log(`Interaction ${icount}: TTS -> TWILIO: ${label}`.blue);
+
+      streamService.buffer(responseIndex, audio);
+    });
+
+    streamService.on("audiosent", (markLabel) => {
+      marks.push(markLabel);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/call", async (req, res) => {
+  try {
+    const phoneNumber = req?.body?.phone_number;
+
+    if (!phoneNumber) {
+      return res.status(400).json({
+        isSuccess: false,
+        message: "Phone Number is required!",
+      });
+    }
+
+    await makeOutBoundCall(req?.body?.phone_number);
+
+    res.status(200).json({
+      isSuccess: true,
+      message: "Success",
+    });
+  } catch (error) {
+    console.log("🚀 ~ app.get ~ error:", error);
+    res.status(500).json({
+      isSuccess: false,
+      message: "Internal Server Error",
+    });
+  }
+});
+
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
